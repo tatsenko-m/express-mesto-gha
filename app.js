2422c5d43a54f5bb8d0db553f49525fca173ce91
tatsenko-m <tatsenko.m@yandex.ru>
2023-06-06 23:30:15 +0300

    feat: подключить mongoose и создать папку models

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = require('./routes/index');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 app.use(express.json());
 
