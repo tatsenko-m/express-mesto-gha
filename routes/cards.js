efbe7e3996b609e132b68d537c849181770e51f4
tatsenko-m <tatsenko.m@yandex.ru>
2023-06-06 22:12:32 +0300

    feat: создать структуру роутов

---
+++ routes/cards.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { getCards, createCard, deleteCardById, likeCard, unlikeCard } = require('../controllers/cards');
+
+router.get('/', getCards);
+
+router.post('/', createCard);
+
+router.delete('/:cardId', deleteCardById);
+
+router.put('/:cardId/likes', likeCard);
+
+router.delete('/:cardId/likes', unlikeCard);
+
+module.exports = router;
