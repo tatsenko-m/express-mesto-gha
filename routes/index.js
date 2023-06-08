550ca96f7bf9e5e4cf09ed807ede4fe27a401e9a
tatsenko-m <tatsenko.m@yandex.ru>
2023-06-08 12:45:58 +0300

    feat: добавить обработчик всех неправильных путей

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
+const { NOT_FOUND } = require('../constants/errorStatus');
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
+router.use((req, res) => {
+  res.status(NOT_FOUND).json({ message: 'Не найдено' });
+});
 
 module.exports = router;
