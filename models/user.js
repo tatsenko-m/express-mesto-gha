6c4bf5c8cca75db67e4a69d6f23185a7e896f9e3
tatsenko-m <tatsenko.m@yandex.ru>
2023-06-20 16:04:36 +0300

    feat: добавить email и password к схеме пользователя

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,6 +17,19 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (field) => validator.isEmail(field),
+      message: 'Некорректный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    requred: true,
   },
 });
 
