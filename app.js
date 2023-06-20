bf7d8d133f7ed71aecff447a43f8d3cc65239d1e
tatsenko-m <tatsenko.m@yandex.ru>
2023-06-20 22:39:21 +0300

    fix: удалить хардкод

--- app.js
+++ app.js
@@ -13,14 +13,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(express.json());
 
-/* app.use((req, res, next) => {
-  req.user = {
-    _id: '64805f5d3ba9d938dda5f3e8',
-  };
-
-  next();
-}); */
-
 app.use(cookieParser());
 
 app.post('/signin', login);
