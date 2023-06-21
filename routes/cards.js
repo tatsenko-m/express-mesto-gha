e55cb907de4daf79ebd08b0a13c7b883d7289c5b
tatsenko-m <tatsenko.m@yandex.ru>
2023-06-21 19:56:06 +0300

    feat: добавить валидацию запросов в файл роутов cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,16 +1,35 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getCards, createCard, deleteCardById, likeCard, unlikeCard,
 } = require('../controllers/cards');
+const { regex } = require('../constants/regex');
 
 router.get('/', getCards);
 
-router.post('/', createCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(regex),
+  }),
+}), createCard);
 
-router.delete('/:cardId', deleteCardById);
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24).required(),
+  }),
+}), deleteCardById);
 
-router.put('/:cardId/likes', likeCard);
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24).required(),
+  }),
+}), likeCard);
 
-router.delete('/:cardId/likes', unlikeCard);
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24).required(),
+  }),
+}), unlikeCard);
 
 module.exports = router;
