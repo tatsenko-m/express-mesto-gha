c8410b73e921d2ccc6145275bf9a29863cea7994
tatsenko-m <tatsenko.m@yandex.ru>
2023-06-21 19:15:25 +0300

    feat: добавить константу regex и исправить роутер

---
+++ constants/regex.js
@@ -0,0 +1,1 @@
+module.exports.regex = /^(https?:\/\/)?(www\.)?[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]+$/;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
-const { NOT_FOUND } = require('../constants/errorStatus');
+const NotFoundError = require('../errors/NotFoundError');
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
-router.use((req, res) => {
-  res.status(NOT_FOUND).json({ message: 'Не найдено' });
+router.use('*', (req, res, next) => {
+  next(new NotFoundError('Не найдено'));
 });
 
 module.exports = router;
