08439f149cc75b527683387b132685f508adf331
tatsenko-m <tatsenko.m@yandex.ru>
2023-06-21 20:05:31 +0300

    feat: добавить валидацию запросов в файл роутов users

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,31 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getUserById, getCurrentUserInfo, updateUserInfo, updateUserAvatar,
 } = require('../controllers/users');
+const { regex } = require('../constants/regex');
 
 router.get('/', getUsers);
 
 router.get('/me', getCurrentUserInfo);
 
-router.get('/:userId', getUserById);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().hex().length(24).required(),
+  }),
+}), getUserById);
 
-router.patch('/me', updateUserInfo);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUserInfo);
 
-router.patch('/me/avatar', updateUserAvatar);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().pattern(regex),
+  }),
+}), updateUserAvatar);
 
 module.exports = router;
