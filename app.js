395d85a2bd3a115b4706c7a7e13152550969cbf1
tatsenko-m <tatsenko.m@yandex.ru>
2023-06-21 20:21:54 +0300

    feat: добавить валидацию запросов для роутов входа и регистрации

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const router = require('./routes/index');
+const { regex } = require('./constants/regex');
 
 const app = express();
 
@@ -15,14 +17,30 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(regex),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
 
 app.use(auth);
 
 app.use(router);
 
 app.listen(3000);
+
+app.use(errors());
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
